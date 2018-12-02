64729b63375a575c233414fd88be88da0c54658b
hamed akhlaghi <akhlaghi65@gmail.com>
2018-12-02 13:24:35 +0330

    Before relaese swift 3

--- oath/ChallengeResponse.swift
+++ oath/ChallengeResponse.swift
@@ -9,7 +9,6 @@ import CryptoSwift
 import Foundation
     public class ChallengeResponse: ChallengeResponseDelegate {
        
-
                                 //  0  1   2     3    4        5        6       7         8
         
         private let DIGITS_POWER = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000]
